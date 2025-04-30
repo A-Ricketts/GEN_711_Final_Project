b7b164e05a03c82f0831fbc329d79aa69ce0c0a6
Andrew Ricketts <amr1230@ron.sr.unh.edu>
2025-04-29 20:27:32 -0400

    April 25 update

--- script.sh
+++ script.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
-conda activate genomics
+source activate genomics
 
 #estimate of coverage of reads
 
 data="~/Final_Project/Data/"
 
-##something is wrong!!
 for item in ~/Final_Project/Data/*R1*
   do
    num_reads=$(zgrep -c "^@" $item)
-   bp=$($num_reads * 250 * 2 / 7000000)
+   bp=$(($num_reads * 250 * 2 / 7000000))
    if [ $bp -ge 70 ]
     then
      echo $item has good coverage
@@ -18,6 +17,8 @@ for item in ~/Final_Project/Data/*R1*
      echo $item does not have good coverage
    fi
 done
+
+sleep 5
 
 #read quality
 
@@ -27,7 +28,7 @@ rawreads="~/Final_Project/fastqc_raw-reads"
 
 for item in $data
  do
-  fastqc $item $rawreads
+  fastqc $item -o $rawreads
 done
 
 sleep 5
@@ -37,5 +38,16 @@ sleep 5
 mkdir ~/Final_Project/trimmed_reads
 
 trimreads="~/Final_Project/trimmed_reads"
+trimqc="~/Final_Project/trimmed_reads_fastqc"
 
+for item in $data
+ do
+  trim_file=$(trim_scriptV2.sh $item)
+  mv $trim_file $trimreads
+  fastqc $trim_file -o $trimqc
+done
+
+sleep 5
+
+#genome assembly
 
--- testing_script.sh
+++ testing_script.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 
+source activate genomics
+
+#estimate of coverage of reads
+
+data="~/Final_Project/Data/"
+
+##something is wrong!!
 for item in ~/Final_Project/Data/*R1*
   do
    num_reads=$(zgrep -c "^@" $item)
-   bp=$($num_reads * 250 * 2 / 7000000) 
+   bp=$(($num_reads * 250 * 2 / 7000000))
    if [ $bp -ge 70 ]
-    then 
+    then
      echo $item has good coverage
-    else 
+    else
      echo $item does not have good coverage
    fi
 done
+
