5140e8fa806e9b5ed78187ae40cd4704fc20f638
Andrew Ricketts <amr1230@ron.sr.unh.edu>
2025-05-06 00:31:10 -0400

    saving nohup outputs test run 6

--- testing_script.sh
+++ testing_script.sh
@@ -3,7 +3,9 @@ source activate genomics
 
 data=$1
 
-for item in $data*
-do
-echo $item
+for item in $data*R1*
+ do
+  reverse=$(ls $data* | grep -A1 "$item" | tail -n1)
+  echo $item
+  echo $reverse
 done
